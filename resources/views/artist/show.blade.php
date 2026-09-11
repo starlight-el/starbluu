b7fba804238f7097030fbc0c863c2e257e86fdba
ynelfika <ynelfikar255@gmail.com>
2026-09-11 17:47:43 +0700

    feat: redesign artist detail page (Day 19)

--- resources/views/artist/show.blade.php
+++ resources/views/artist/show.blade.php
@@ -1,64 +1,86 @@
 @extends('layouts.app')
 
+@section('body-class', 'starbluu-theme')
+
 @section('content')
 <div class="container">
 
     @php
+        $tour = $artist->tours->first();
         $backUrl = match(request('from')) {
             'tour' => route('tour.index'),
             'world-tour' => route('tour.world'),
             default => route('landing'),
         };
     @endphp
-    <a href="{{ $backUrl }}">&lt; Kembali</a>
 
-    <div class="row mt-3">
-        <div class="col-md-8">
-            <h2>{{ $artist->nama_grup }}</h2>
-            <p>{{ $artist->deskripsi }}</p>
+    <a href="{{ $backUrl }}" class="back-link">&lt; Kembali</a>
+
+    <div class="row artist-hero">
+        <div class="col-md-6 mb-4 mb-md-0">
+            <div class="artist-banner"
+                @if ($tour && $tour->foto_banner_detail) style="background-image: url('{{ asset('storage/' . $tour->foto_banner_detail) }}');" @endif>
+                @if ($tour)
+                    <div class="artist-banner-caption">
+                        <h1>{{ $tour->nama_tour }}</h1>
+                    </div>
+                @endif
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="artist-info-box">
+                <h2>{{ $artist->nama_grup }}</h2>
+                <p>{{ $artist->deskripsi }}</p>
+            </div>
+
+            <h4 class="tour-subtitle">Members</h4>
+            <div class="row member-list">
+                @forelse ($artist->artistMembers as $member)
+                    <div class="col-4 col-md-3 member-item">
+                        <div class="member-avatar"
+                            @if ($member->foto_member) style="background-image: url('{{ asset('storage/' . $member->foto_member) }}');" @endif>
+                            @unless ($member->foto_member)
+                                {{ strtoupper(substr($member->nama_member, 0, 1)) }}
+                            @endunless
+                        </div>
+                        <p>{{ $member->nama_member }}</p>
+                    </div>
+                @empty
+                    <p class="text-muted">Belum ada data member.</p>
+                @endforelse
+            </div>
         </div>
     </div>
 
-    <h4 class="mt-4">Members</h4>
-    <div class="row">
-        @foreach ($artist->artistMembers as $member)
-            <div class="col-md-3 text-center mb-3">
-                <p>{{ $member->nama_member }}</p>
-            </div>
-        @endforeach
-    </div>
+    <h4 class="tour-dates-title mt-5">Tour Dates and Tickets</h4>
 
-    <h4 class="mt-4">Tour Dates and Tickets</h4>
-
-    @foreach ($artist->tours as $tour)
-        <h5>{{ $tour->nama_tour }}</h5>
-
-        @foreach ($tour->jadwals as $jadwal)
-            <div class="card mb-3">
-                <div class="card-body d-flex justify-content-between align-items-center">
-                    <div>
-                        <p class="mb-1">{{ $jadwal->kota }} - {{ $jadwal->venue }}</p>
-                        <p class="mb-0 text-muted">
-                            {{ \Carbon\Carbon::parse($jadwal->tanggal)->format('Y.m.d') }}
-                            @if ($jadwal->jam)
-                                {{ \Carbon\Carbon::parse($jadwal->jam)->format('H:i') }} {{ $jadwal->timezone }}
-                            @endif
-                        </p>
-                    </div>
-
-                    @php
-                        $totalKuota = $jadwal->ticketTiers->sum('kuota');
-                    @endphp
-
+    <div class="jadwal-list">
+        @forelse ($tour->jadwals ?? [] as $jadwal)
+            @php
+                $totalKuota = $jadwal->ticketTiers->sum('kuota');
+            @endphp
+            <div class="jadwal-row">
+                <div class="jadwal-kota">{{ $jadwal->kota }}</div>
+                <div class="jadwal-venue">{{ $jadwal->venue }}</div>
+                <div class="jadwal-date">
+                    {{ \Carbon\Carbon::parse($jadwal->tanggal)->format('Y.m.d') }}
+                    @if ($jadwal->jam)
+                        {{ \Carbon\Carbon::parse($jadwal->jam)->format('H:i') }} {{ $jadwal->timezone }}
+                    @endif
+                </div>
+                <div class="jadwal-action">
                     @if ($totalKuota > 0)
-                        <a href="{{ route('tickettier.show', ['jadwalId' => $jadwal->id, 'from' => request('from')]) }}" class="btn btn-dark">GET TICKETS</a>
+                        <a href="{{ route('tickettier.show', ['jadwalId' => $jadwal->id, 'from' => request('from')]) }}" class="btn-accent">GET TICKETS</a>
                     @else
-                        <button class="btn btn-secondary" disabled>SOLD OUT</button>
+                        <span class="btn-soldout">SOLD OUT</span>
                     @endif
                 </div>
             </div>
-        @endforeach
-    @endforeach
+        @empty
+            <p class="text-muted">Belum ada jadwal tour.</p>
+        @endforelse
+    </div>
 
 </div>
 @endsection
